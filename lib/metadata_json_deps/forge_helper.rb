43d30715791c472374bcd381e50826d9c4b37528
lionce <loredana.ionce@puppet.com>
2019-01-14 14:33:42 +0200

    (FM-7669) - Check if a dependency has been deprecated

--- lib/metadata_json_deps/forge_helper.rb
+++ lib/metadata_json_deps/forge_helper.rb
@@ -32,6 +32,30 @@ module MetadataJsonDeps
       metadata
     end
 
+    def get_metadata_json_uri(module_name)
+      uri = URI.parse(ENV['METADATA_JSON_URI_BASE'] + module_name)
+      request = Net::HTTP::Get.new(uri.to_s)
+      request.content_type = 'application/json'
+      req_options = {
+          use_ssl: uri.scheme == 'https',
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      response.body
+    end
+
+    def check_deprecated_at(module_name)
+      module_name = module_name.sub('/', '-')
+      meta_complete = get_metadata_json_uri(module_name)
+      details = JSON.parse(meta_complete)
+      if details.has_key? 'deprecated_at'
+        details['deprecated_at'] != nil
+      end
+    end
+
     private
 
     def get_mod(name)
--- lib/metadata_json_deps/runner.rb
+++ lib/metadata_json_deps/runner.rb
@@ -17,14 +17,24 @@ module MetadataJsonDeps
     end
 
     def run
-
       @updated_module = @updated_module.sub('-', '/')
       message = "Comparing modules against *#{@updated_module}* version *#{@updated_module_version}*\n\n"
+      if check_deprecated(@forge.get_current_version(@updated_module)) || @forge.check_deprecated_at(@updated_module)
+        message += "The module you are comparing against #{@updated_module.upcase} is DEPRECATED.\n" #if check_deprecated(@forge.get_current_version(@updated_module)) || @forge.check_deprecated_at(@updated_module)
+        puts message
+        post_to_slack(message) if @use_slack
+        post_to_logs(message) if @logs_file
+        exit
+      end
       @module_names.each do |module_name|
         message += "Checking *#{module_name}*\n"
         metadata = @forge.get_metadata_json(module_name)
+        post_to_logs("\n\n") if @logs_file
+        post_to_logs(metadata) if @logs_file
         checker = MetadataJsonDeps::MetadataChecker.new(metadata, @forge, @updated_module, @updated_module_version)
         dependencies = checker.dependencies
+
+        message += "The checked module #{module_name.upcase} is DEPRECATED.\n" if check_deprecated(@forge.get_current_version(module_name)) || @forge.check_deprecated_at(module_name)
 
         if dependencies.empty?
           message += "\tNo dependencies listed\n\n"
@@ -41,6 +51,7 @@ module MetadataJsonDeps
             allMatch = false
             message += "\t#{dependency} (#{constraint}) *doesn't match* #{current}\n"
           end
+          message += "\tThe dependency module #{dependency.upcase} is DEPRECATED.\n" if check_deprecated(current) || @forge.check_deprecated_at(module_name)
         end
 
         message += "\tAll dependencies match\n" if allMatch
@@ -51,6 +62,10 @@ module MetadataJsonDeps
       post_to_slack(message) if @use_slack
       post_to_logs(message) if @logs_file
     rescue Interrupt
+    end
+
+    def check_deprecated(version)
+      version.to_s.eql?('999.999.999') || version.to_s.eql?('99.99.99')
     end
 
     def return_modules(filename)
