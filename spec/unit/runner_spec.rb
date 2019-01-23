2247257760d9ba410492d0292c6066864c535d19
lionce <loredana.ionce@puppet.com>
2019-01-23 10:21:19 +0200

    restructured spec folder

--- spec/unit/runner_spec.rb
+++ spec/unit/runner_spec.rb
@@ -1,35 +1,41 @@
 require 'metadata_json_deps'
 
 describe 'compare_dependencies' do
-  managed_modules = File.join(File.expand_path(File.dirname(File.dirname(__FILE__))), 'unit/managed_modules.yaml')
+  managed_modules = File.join(File.expand_path(File.dirname(File.dirname(__FILE__))), 'fixtures/managed_modules.yaml')
   module_name="puppetlabs-stdlib"
   version = "10.0.0"
   verbose = false
   use_slack = false
-  logs_file = File.join(File.expand_path(File.dirname(File.dirname(__FILE__))), 'unit/logs.log')
+  logs_file = File.join(File.expand_path(File.dirname(File.dirname(__FILE__))), 'fixtures/logs.log')
 
   after :each do
     FileUtils.rm(logs_file) if File.exists?(logs_file)
   end
 
-  describe 'check task compare_dependencies with valid arguments' do
+  context 'check compare_dependencies task with valid arguments' do
     it 'is expected to run without errors when all arguments are valid' do
       expect { MetadataJsonDeps::Runner.run(managed_modules, module_name, version, verbose, use_slack, logs_file) }.not_to raise_error
       expect(File.exist?(logs_file)).to be true
     end
   end
 
-  describe 'check task with mandatory arguments: managed_modules, module_name, version, verbose' do
+  context 'compare_dependencies task with mandatory arguments: managed_modules, module_name, version, verbose' do
     it 'is expected to run without error when all mandatory arguments are valid' do
       expect { MetadataJsonDeps::Runner.run(managed_modules, module_name, version, verbose) }.not_to raise_error
       expect(File.exist?(logs_file)).to be false
     end
   end
 
-  describe 'compare_dependencies with invalid arguments' do
-    it 'is expected to raise an error when an argument is incorrect' do
-      expect { MetadataJsonDeps::Runner.run('invalid_path', 'puppetlabs-stdlib', '1.0.0', verbose) }.to raise_error(RuntimeError, "File 'invalid_path' is empty/does not exist")
+  context 'compare_dependencies task with invalid arguments' do
+    it 'is expected to raise an error when managed_modules is not found' do
+      expect { MetadataJsonDeps::Runner.run('invalid path', 'puppetlabs-stdlib', '1.0.0', verbose) }.to raise_error(RuntimeError, "File 'invalid path' is empty/does not exist")
+    end
+
+    it 'is expected to raise an error when module name could not be found in the Forge' do
       expect { MetadataJsonDeps::Runner.run(managed_modules, 'invalid_name', '1.0.0', verbose) }.to raise_error(RuntimeError, "Error: Verify *invalid_name* exists on Puppet Forge! Verify semantic versioning syntax *1.0.0*. \n")
+    end
+
+    it 'is expected to raise an error when version has an invalid syntax' do
       expect { MetadataJsonDeps::Runner.run(managed_modules, 'puppetlabs-stdlib', '1.00', verbose) }.to raise_error(RuntimeError, "Error: Verify *puppetlabs-stdlib* exists on Puppet Forge! Verify semantic versioning syntax *1.00*. \n")
     end
   end
