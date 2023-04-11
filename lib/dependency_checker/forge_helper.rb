4b4cbb66b47f86bb6acc570dc6527231cc9df662
Craig Gumbley <craiggumbley@gmail.com>
2023-04-11 12:28:56 +0100

    (CONT-810) Correct Style/ClassAndModuleChildren

--- lib/dependency_checker/forge_helper.rb
+++ lib/dependency_checker/forge_helper.rb
@@ -4,78 +4,80 @@ require 'puppet_forge'
 require 'semantic_puppet'
 
 # Helper class for fetching data from the Forge and perform some basic operations
-class DependencyChecker::ForgeHelper
-  def initialize(cache = {}, forge_hostname = nil, forge_token = nil)
-    @cache = cache
-    PuppetForge.host = forge_hostname unless forge_hostname.nil?
-    PuppetForge::Connection.authorization = forge_token unless forge_token.nil?
-  end
-
-  # Retrieve current version of module
-  # @return [SemanticPuppet::Version]
-  def get_current_version(module_name)
-    module_name = module_name.sub('/', '-')
-    version = nil
-    version = get_version(@cache[module_name]) if @cache.key?(module_name)
-
-    if !version && check_module_exists(module_name)
-      version = get_version(get_module_data(module_name))
+module DependencyChecker
+  class ForgeHelper
+    def initialize(cache = {}, forge_hostname = nil, forge_token = nil)
+      @cache = cache
+      PuppetForge.host = forge_hostname unless forge_hostname.nil?
+      PuppetForge::Connection.authorization = forge_token unless forge_token.nil?
     end
 
-    version
-  end
+    # Retrieve current version of module
+    # @return [SemanticPuppet::Version]
+    def get_current_version(module_name)
+      module_name = module_name.sub('/', '-')
+      version = nil
+      version = get_version(@cache[module_name]) if @cache.key?(module_name)
 
-  # Retrieve module data from Forge
-  # @return [Hash] Hash containing JSON response from Forge
-  def get_module_data(module_name)
-    module_name = module_name.sub('/', '-')
-    module_data = @cache[module_name]
-    begin
-      @cache[module_name] = module_data = PuppetForge::Module.find(module_name) unless module_data
-    rescue Faraday::ClientError
-      return nil
+      if !version && check_module_exists(module_name)
+        version = get_version(get_module_data(module_name))
+      end
+
+      version
     end
 
-    module_data
-  end
+    # Retrieve module data from Forge
+    # @return [Hash] Hash containing JSON response from Forge
+    def get_module_data(module_name)
+      module_name = module_name.sub('/', '-')
+      module_data = @cache[module_name]
+      begin
+        @cache[module_name] = module_data = PuppetForge::Module.find(module_name) unless module_data
+      rescue Faraday::ClientError
+        return nil
+      end
 
-  # Retrieve module from Forge
-  # @return [PuppetForge::Module]
-  def check_module_exists(module_name)
-    !get_module_data(module_name).nil?
-  end
+      module_data
+    end
 
-  # Check if a module is deprecated from data fetched from the Forge
-  # @return [Boolean] boolean result stating whether module is deprecated
-  def check_module_deprecated(module_name)
-    module_name = module_name.sub('/', '-')
-    module_data = get_module_data(module_name)
-    version = get_current_version(module_name)
-    version.to_s.eql?('999.999.999') || version.to_s.eql?('99.99.99') || !module_data.attribute('deprecated_at').nil?
-  end
+    # Retrieve module from Forge
+    # @return [PuppetForge::Module]
+    def check_module_exists(module_name)
+      !get_module_data(module_name).nil?
+    end
 
-  # Gets a list of all modules in a namespace, optionally filtered by endorsement.
-  # @param [String] namespace The namespace to search
-  # @param [String] endorsement to filter by (supported/approved/partner)
-  # @return [Array] list of modules
-  def modules_in_namespace(namespace, endorsement = nil)
-    modules = PuppetForge::Module.where(
-                :owner => namespace, # rubocop:disable Layout/FirstArgumentIndentation
-                :hide_deprecated => true,
-                :module_groups => 'base pe_only',
-                :endorsements => endorsement,
-              )
+    # Check if a module is deprecated from data fetched from the Forge
+    # @return [Boolean] boolean result stating whether module is deprecated
+    def check_module_deprecated(module_name)
+      module_name = module_name.sub('/', '-')
+      module_data = get_module_data(module_name)
+      version = get_current_version(module_name)
+      version.to_s.eql?('999.999.999') || version.to_s.eql?('99.99.99') || !module_data.attribute('deprecated_at').nil?
+    end
 
-    raise "No modules found for #{namespace}." if modules.total.zero?
+    # Gets a list of all modules in a namespace, optionally filtered by endorsement.
+    # @param [String] namespace The namespace to search
+    # @param [String] endorsement to filter by (supported/approved/partner)
+    # @return [Array] list of modules
+    def modules_in_namespace(namespace, endorsement = nil)
+      modules = PuppetForge::Module.where(
+                  :owner => namespace, # rubocop:disable Layout/FirstArgumentIndentation
+                  :hide_deprecated => true,
+                  :module_groups => 'base pe_only',
+                  :endorsements => endorsement,
+                )
 
-    modules.unpaginated.map { |m| m.slug }
-  end
+      raise "No modules found for #{namespace}." if modules.total.zero?
 
-  private
+      modules.unpaginated.map { |m| m.slug }
+    end
 
-  def get_version(module_data)
-    return SemanticPuppet::Version.parse('999.999.999') unless module_data.current_release
+    private
 
-    SemanticPuppet::Version.parse(module_data.current_release.version)
+    def get_version(module_data)
+      return SemanticPuppet::Version.parse('999.999.999') unless module_data.current_release
+
+      SemanticPuppet::Version.parse(module_data.current_release.version)
+    end
   end
 end
--- lib/dependency_checker/metadata_checker.rb
+++ lib/dependency_checker/metadata_checker.rb
@@ -3,35 +3,37 @@
 require 'semantic_puppet'
 
 # Checks dependencies of passed in metadata and performs checks to verify constraints
-class DependencyChecker::MetadataChecker
-  def initialize(metadata, forge, updated_module, updated_module_version)
-    @metadata = metadata
-    @forge = forge
-    @updated_module = updated_module.sub('-', '/') if updated_module
-    @updated_module_version = updated_module_version if updated_module_version
-  end
-
-  # Perform constraint comparisons of dependencies based on their latest version, and also
-  # override any occurance of @updated_module with @updated_module_version
-  # @return [Map] a map of dependencies along with their constraint, current version and whether they satisfy the constraint
-  def check_dependencies
-    fetch_module_dependencies.map do |dependency, constraint|
-      dependency = dependency.sub('-', '/')
-      current = (dependency == @updated_module) ? SemanticPuppet::Version.parse(@updated_module_version) : @forge.get_current_version(dependency)
-      [dependency, constraint, current, constraint.include?(current)]
+module DependencyChecker
+  class MetadataChecker
+    def initialize(metadata, forge, updated_module, updated_module_version)
+      @metadata = metadata
+      @forge = forge
+      @updated_module = updated_module.sub('-', '/') if updated_module
+      @updated_module_version = updated_module_version if updated_module_version
     end
-  end
 
-  private
+    # Perform constraint comparisons of dependencies based on their latest version, and also
+    # override any occurance of @updated_module with @updated_module_version
+    # @return [Map] a map of dependencies along with their constraint, current version and whether they satisfy the constraint
+    def check_dependencies
+      fetch_module_dependencies.map do |dependency, constraint|
+        dependency = dependency.sub('-', '/')
+        current = (dependency == @updated_module) ? SemanticPuppet::Version.parse(@updated_module_version) : @forge.get_current_version(dependency)
+        [dependency, constraint, current, constraint.include?(current)]
+      end
+    end
 
-  # Retrieve dependencies from @metedata
-  # @return [Map] a map with the name of the dependency and its constraint
-  def fetch_module_dependencies
-    return [] unless @metadata[:dependencies]
+    private
 
-    @metadata[:dependencies].map do |dep|
-      constraint = dep[:version_requirement] || '>= 0'
-      [dep[:name], SemanticPuppet::VersionRange.parse(constraint)]
+    # Retrieve dependencies from @metedata
+    # @return [Map] a map with the name of the dependency and its constraint
+    def fetch_module_dependencies
+      return [] unless @metadata[:dependencies]
+
+      @metadata[:dependencies].map do |dep|
+        constraint = dep[:version_requirement] || '>= 0'
+        [dep[:name], SemanticPuppet::VersionRange.parse(constraint)]
+      end
     end
   end
 end
