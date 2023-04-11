ee41b8bf32cdc599f92c2a9daacdb13bebb3818b
Craig Gumbley <craiggumbley@gmail.com>
2023-04-11 12:28:04 +0100

    (CONT-810) Correct Style/BlockDelimiters

--- spec/unit/runner_spec.rb
+++ spec/unit/runner_spec.rb
@@ -11,60 +11,60 @@ describe 'compare_dependencies' do
 
   context 'run executable with namespace' do
     it 'is expected to run without errors' do
-      expect {
+      expect do
         runner = DependencyChecker::Runner.new(verbose)
         runner.resolve_from_namespace('puppetlabs', 'supported')
         runner.run
-      }.not_to raise_error
+      end.not_to raise_error
     end
   end
 
   context 'run executable with list of modules' do
     it 'when passed a file of modules' do
-      expect {
+      expect do
         runner = DependencyChecker::Runner.new(verbose)
         runner.resolve_from_path(managed_modules_file)
         runner.run
-      }.not_to raise_error
+      end.not_to raise_error
     end
 
     it 'when passed a url with modules' do
-      expect {
+      expect do
         runner = DependencyChecker::Runner.new(verbose)
         runner.resolve_from_path(managed_modules_url)
         runner.run
-      }.not_to raise_error
+      end.not_to raise_error
     end
   end
 
   context 'run executable with invalid arguments' do
     it 'is expected to raise an error when managed_modules is not found' do
       error_message = '*Error:* Ensure *invalid_path* is a valid file path or URL'
-      expect {
+      expect do
         runner = DependencyChecker::Runner.new(verbose)
         runner.resolve_from_path('invalid_path')
         runner.run
-      }.to raise_error(RuntimeError, error_message)
+      end.to raise_error(RuntimeError, error_message)
     end
 
     it 'is expected to raise an error when module name could not be found in the Forge' do
       error_message = '*Error:* Could not find *invalid_name* on Puppet Forge! Ensure updated_module argument is valid.'
-      expect {
+      expect do
         runner = DependencyChecker::Runner.new(verbose)
         runner.resolve_from_path(managed_modules_file)
         runner.override = ['invalid_name', '1.0.0']
         runner.run
-      }.to raise_error(RuntimeError, error_message)
+      end.to raise_error(RuntimeError, error_message)
     end
 
     it 'is expected to raise an error when version has an invalid syntax' do
       error_message = '*Error:* Verify semantic versioning syntax *1.00* of updated_module_version argument.'
-      expect {
+      expect do
         runner = DependencyChecker::Runner.new(verbose)
         runner.resolve_from_path(managed_modules_file)
         runner.override = ['puppetlabs-stdlib', '1.00']
         runner.run
-      }.to raise_error(RuntimeError, error_message)
+      end.to raise_error(RuntimeError, error_message)
     end
   end
 end
